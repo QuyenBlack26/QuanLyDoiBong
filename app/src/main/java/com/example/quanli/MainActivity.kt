5ed4472c4b4c4d71f50ded32c79a802f5800c4b2
Quyến Black <havanan.8ph@gmail.com>
2026-07-10 17:22:11 +0700

    sua lai cau hinh giao dien adroi

--- app/src/main/java/com/example/quanli/MainActivity.kt
+++ app/src/main/java/com/example/quanli/MainActivity.kt
@@ -4,14 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.quanli.adapter.NewsAdapter
@@ -46,7 +43,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         
         val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
@@ -55,18 +51,9 @@ class MainActivity : AppCompatActivity() {
             onBackPressedDispatcher.onBackPressed()
         }
         
-        setupEdgeToEdge()
         setupRecyclerView()
         observeViewModel()
         setupFab()
-    }
-
-    private fun setupEdgeToEdge() {
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 
     private fun setupRecyclerView() {
